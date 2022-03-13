21d23823134d5a68b433efe87e1c91d9d4fed17b
Alejandro <alejandrovasquezesc@gmail.com>
2022-03-12 23:48:08 -0600

    docs added

---
+++ docs/precache-manifest.df3760354a9412411767b98589d8c614.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59d757c1d24e727b4726c14b8e76eebd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68cf09f510f0a852a0fe",
+    "url": "/static/css/main.f2ea8714.chunk.css"
+  },
+  {
+    "revision": "c56f48dc0e951dd3238b",
+    "url": "/static/js/2.bb4c6519.chunk.js"
+  },
+  {
+    "revision": "5c12ca2df883486c500ebd423c983b56",
+    "url": "/static/js/2.bb4c6519.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "68cf09f510f0a852a0fe",
+    "url": "/static/js/main.5c332f4e.chunk.js"
+  },
+  {
+    "revision": "1f9b80c10cb2174ac7b3",
+    "url": "/static/js/runtime-main.8c0f0de7.js"
+  }
+]);
